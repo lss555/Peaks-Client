674f77d7eff7f943a7149ffacc53d2cea30be358
lss555 <chrisheibel7@gmail.com>
2020-08-10 12:46:31 -0600

    / dropped of config url

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://ancient-taiga-30748.herokuapp.com/',
+  production: 'https://ancient-taiga-30748.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
